f3ab6ae5d41477f81380471d8ae00711b538b26c
Jonas Priestyard <6285509+Fillipuster@users.noreply.github.com>
2018-05-22 11:39:12 +0200

    Finished prototype FData. Check up on those ON DUPLICATE KEY sql statements.

--- lua/seriesql/database.lua
+++ lua/seriesql/database.lua
@@ -8,6 +8,16 @@ function SSQL.ErrorPrint(origin, error)
 	//print("SSQL Error from [" .. origin .. "] reads:\n" .. error);
 end
 
+local function SSQL.CheckDatabase(origin)
+	if (SSQL.database:status() != mysqloo.DATABASE_CONNECTED) then
+		MsgC(Color(255, 100, 100), "SSQL Database connection invalid/lost [", origin, "]. Attempting to reconnect.");
+		SSQL.Connect();
+		return false;
+	else
+		return true;
+	end
+end
+
 /*---------------------------------------------------------------------------
 Database Setup/Preperation
 ---------------------------------------------------------------------------*/
@@ -34,13 +44,7 @@ end
 Functions
 ---------------------------------------------------------------------------*/
 function SSQL.CreatePlayer(ply)
-	if (!ply:IsValid() || !ply:IsPlayer()) then return; end
-
-	if (SSQL.database:status() != mysqloo.DATABASE_CONNECTED) then
-		SSQL.ErrorPrint("SSQL.CreatePlayer", "Database connection invalid/lost. Attempting to reconnect.");
-		SSQL.Connect();
-		return;
-	end
+	if (!ply:IsValid() || !ply:IsPlayer() || !SSQL.CheckDatabase("SSQL.CreatePlayer")) then return; end
 
 	SSQL.database:PrepareQuery("INSERT INTO seriesql_player (steamid64) VALUES (?);", {ply:SteamID64()}, function(_, status, data)
 		if (!status) then
@@ -52,13 +56,7 @@ function SSQL.CreatePlayer(ply)
 end
 
 function SSQL.LoadPlayerData(ply)
-	if (!ply:IsValid() || !ply:IsPlayer()) then return; end
-
-	if (SSQL.database:status() != mysqloo.DATABASE_CONNECTED) then
-		SSQL.ErrorPrint("SSQL.LoadPlayerData", "Database connection invalid/lost. Attempting to reconnect.");
-		SSQL.Connect();
-		return;
-	end
+	if (!ply:IsValid() || !ply:IsPlayer() || !SSQL.CheckDatabase("SSQL.LoadPlayerData")) then return; end
 
 	SSQL.database:PrepareQuery("SELECT data FROM seriesql_player WHERE steamid64 = ?;", {ply:SteamID64()}, function(_, status, data)
 		if (!status) then
@@ -74,7 +72,7 @@ function SSQL.LoadPlayerData(ply)
 end
 
 function SSQL.SavePlayerData(ply)
-	if (!ply:IsValid() || !ply:IsPlayer()) then return; end
+	if (!ply:IsValid() || !ply:IsPlayer() || !SSQL.CheckDatabase("SSQL.SavePlayerData")) then return; end
 
 	if (SSQL.database:status() != mysqloo.DATABASE_CONNECTED) then
 		SSQL.ErrorPrint("SSQL.SavePlayerData", "Database connection invalid/lost. Attempting to reconnect.");
@@ -94,38 +92,66 @@ function SSQL.SavePlayerData(ply)
 end
 
 // FData //
-function SSQL.CreateFloat(name)
-	if (SSQL.database:status() != mysqloo.DATABASE_CONNECTED) then
-		SSQL.ErrorPrint("SSQL.CreateFloat", "Database connection invalid/lost. Attempting to reconnect.");
-		SSQL.Connect();
-		return;
-	end
-
-	SSQL.database:PrepareQuery("INSERT INTO seriesql_float (name) VALUES (?);", {name}, function(_, status, data)
-		if (!status) then
-			SSQL.ErrorPrint("SSQL.CreateFloat", data);
-		else
-			SSQL.float[name].data = SSQL.float[name].data or {};
-		end
-	end)
-end
-
 function SSQL.LoadFloatData()
-	if (SSQL.database:status() != mysqloo.DATABASE_CONNECTED) then
-		SSQL.ErrorPrint("SSQL.LoadFloatData", "Database connection invalid/lost. Attempting to reconnect.");
-		SSQL.Connect();
-		return;
-	end
+	if (!SSQL.CheckDatabase("SSQL.LoadFloatData")) then return; end
 
 	SSQL.database:PrepareQuery("SELECT * FROM seriesql_float;", {}, function(_, status, data)
 		if (!status) then
 			SSQL.ErrorPrint("SSQL.LoadFloatData", data);
 		else
-			if (data[1]) then
-				SSQL.players[ply:SteamID64()].data = util.JSONToTable(data[1].data) or {};
-			else
-				SSQL.CreatePlayer(ply);
+			for _,row in pairs(data) do
+				SSQL.float[row.name].data = util.JSONToTable(row.data);
 			end
 		end
 	end)
 end
+
+function SSQL.SaveFloatData(name)
+	if (!SSQL.CheckDatabase("SSQL.SaveFloatData")) then return; end
+
+	if (SSQL.float[name] && SSQL.float[name].queueSave) then
+		SSQL.database:PrepareQuery("INSERT INTO seriesql_float (name, data) VALUES(?, ?) ON DUPLICATE KEY UPDATE name = ?, data = ?;", {name, util.TableToJSON(SSQL.float[name].data)}, function(_, status, data)
+			if (!status) then
+				SSQL.ErrorPrint("SSQL.SaveFloatData", data);
+			end
+		end)
+	end
+end
+
+function SSQL.SaveAllFloatData()
+	for name,_ in pairs(SSQL.float) do
+		SSQL.SaveFloatData(name);
+	end
+end
+
+
+/*
+function SSQL.SaveAllFloatData()
+	if (!SSQL.CheckDatabase("SSQL.SaveAllFloatData")) then return; end
+
+	SSQL.database:PrepareQuery("SELECT name FROM seriesql_float;", {}, function(_, stauts, data)
+		if (!status) then
+			SSQL.ErrorPrint("SSQL.SaveAllFloatData-00", data);
+		else
+			local databaseNames = {};
+			for _,row in pairs(data) do
+				table.insert(databaseNames, row.name);
+			end
+
+			for name,tab in pairs(SSQL.float) do
+				if (tab.queueSave) then
+					if (table.HasValue(databaseNames, name)) then
+						SSQL.database:PrepareQuery("UPDATE seriesql_float SET data = ? WHERE name = ?;", {util.TableToJSON(tab.data), name}, function(_, status, data)
+							if (!status) then
+								SSQL.ErrorPrint("SSQL.SaveAllFloatData-01", data);
+							end
+						end)
+					else
+						SSQL.database:PrepareQuery("UPDATE ")
+					end
+				end
+			end
+		end
+	end)
+end
+*/
