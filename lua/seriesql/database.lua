751ef43b5b485c52f583c04dbfdc21097cd85244
Jonas Priestyard <jonas@priestyard.dk>
2018-05-22 07:13:15 +0200

    Added database connection checking.

--- lua/seriesql/database.lua
+++ lua/seriesql/database.lua
@@ -10,26 +10,36 @@ end
 /*---------------------------------------------------------------------------
 Database Setup/Preperation
 ---------------------------------------------------------------------------*/
-SSQL.database = mysqloo.CreateDatabase(SSQL.config.hostname, SSQL.config.username, SSQL.config.password, SSQL.config.database/*, SSQL.config.port*/);
-SSQL.database:wait();
+function SSQL.Connect()
+	if (SSQL.database && SSQL.database:status() == mysqloo.DATABASE_CONNECTED) then return; end
 
-SSQL.database:RunQuery("CREATE TABLE IF NOT EXISTS seriesql_player (steamid64 VARCHAR(64), data VARCHAR(255));", function(_, status, data)
-	if (!status) then
-		SSQL.ErrorPrint("seriesql_player table creation", data);
-	end
-end);
+	SSQL.database = mysqloo.CreateDatabase(SSQL.config.hostname, SSQL.config.username, SSQL.config.password, SSQL.config.database/*, SSQL.config.port*/);
+	SSQL.database:wait();
 
-SSQL.database:RunQuery("CREATE TABLE IF NOT EXISTS seriesql_float (name VARCHAR(255), data VARCHAR(255));", function(_, status, data)
-	if (!status) then
-		SSQL.ErrorPrint("seriesql_float table creation", data);
-	end
-end);
+	SSQL.database:RunQuery("CREATE TABLE IF NOT EXISTS seriesql_player (steamid64 VARCHAR(64), data VARCHAR(255));", function(_, status, data)
+		if (!status) then
+			SSQL.ErrorPrint("seriesql_player table creation", data);
+		end
+	end);
+
+	SSQL.database:RunQuery("CREATE TABLE IF NOT EXISTS seriesql_float (name VARCHAR(255), data VARCHAR(255));", function(_, status, data)
+		if (!status) then
+			SSQL.ErrorPrint("seriesql_float table creation", data);
+		end
+	end);
+end
 
 /*---------------------------------------------------------------------------
 Functions
 ---------------------------------------------------------------------------*/
 function SSQL.CreatePlayer(ply)
 	if (!ply:IsValid() || !ply:IsPlayer()) then return; end
+
+	if (SSQL.database:status() != mysqloo.DATABASE_CONNECTED) then
+		SSQL.ErrorPrint("SSQL.LoadPlayerData", "Database connection invalid/lost. Attempting to reconnect.");
+		SSQL.Connect();
+		return;
+	end
 
 	SSQL.database:PrepareQuery("INSERT INTO seriesql_player (steamid64) VALUES (?);", {ply:SteamID64()}, function(_, status, data)
 		if (!status) then
@@ -42,6 +52,12 @@ end
 
 function SSQL.LoadPlayerData(ply)
 	if (!ply:IsValid() || !ply:IsPlayer()) then return; end
+
+	if (SSQL.database:status() != mysqloo.DATABASE_CONNECTED) then
+		SSQL.ErrorPrint("SSQL.LoadPlayerData", "Database connection invalid/lost. Attempting to reconnect.");
+		SSQL.Connect();
+		return;
+	end
 
 	SSQL.database:PrepareQuery("SELECT data FROM seriesql_player WHERE steamid64 = ?;", {ply:SteamID64()}, function(_, status, data)
 		if (!status) then
@@ -58,6 +74,12 @@ end
 
 function SSQL.SavePlayerData(ply)
 	if (!ply:IsValid() || !ply:IsPlayer()) then return; end
+
+	if (SSQL.database:status() != mysqloo.DATABASE_CONNECTED) then
+		SSQL.ErrorPrint("SSQL.LoadPlayerData", "Database connection invalid/lost. Attempting to reconnect.");
+		SSQL.Connect();
+		return;
+	end
 
 	if (SSQL.players[ply:SteamID64()] && SSQL.players[ply:SteamID64()].data && SSQL.players[ply:SteamID64()].queueSave) then
 		SSQL.database:PrepareQuery("UPDATE seriesql_player SET data = ? WHERE steamid64 = ?;", {util.TableToJSON(SSQL.players[ply:SteamID64()].data), ply:SteamID64()}, function(_, status, data)
